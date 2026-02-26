11296ca03f3d3ad2e12df2ad9930b39e5ab64649
meta11 <osyakanasandayo22@gmail.com>
2026-02-26 14:20:46 +0900

    update

--- script.js
+++ script.js
@@ -268,9 +268,8 @@ function initPastPage() {
                 const li = document.createElement('li');
                 li.setAttribute('data-id', j.id);
 
-                if ((j.likes || 0) > (j.dislikes || 0)) {
-                    li.classList.add('white-post');
-                }
+// ソース[3]の該当箇所を以下の一行に変更
+updatePostStyle(li, j.likes || 0, j.dislikes || 0);
 
 // loadMore 関数内の nextItems.forEach ループ内
 const isOwner = currentUser && j.uid === currentUser.uid;
@@ -457,10 +456,23 @@ function createHeart(btn) {
     setTimeout(() => h.remove(), 1000);
 }
 
+// [1][2] の updatePostStyle 関数を以下に置き換え
 function updatePostStyle(li, likes, dislikes) {
-    if (likes > dislikes) {
-        li.classList.add('white-post');
+    const total = (likes || 0) + (dislikes || 0);
+    
+    if (total > 0) {
+      // 比率に応じて動的背景クラスを適用
+      li.classList.add('dynamic-ratio');
+      // 低評価（左側・黒）の割合をパーセントで計算
+      const ratio = (dislikes / total) * 100;
+      // CSS変数 --split-point に値をセット
+      li.style.setProperty('--split-point', `${ratio}%`);
     } else {
-        li.classList.remove('white-post');
+      // 評価が0の場合は特殊なスタイルを解除
+      li.classList.remove('dynamic-ratio');
+      li.style.removeProperty('--split-point');
     }
-}
+  
+    // 以前の「高評価が多い時に白くする」だけのクラスは干渉を防ぐため削除
+    li.classList.remove('white-post');
+  }
